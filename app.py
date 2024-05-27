2f135a1ab935bea69b7ea526745f0afd16403642
jbalooshie <jhboshier@gmail.com>
2024-05-27 14:47:14 -0400

    Add comments throughout.

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ import logging
 import time
 import os
 from datetime import datetime
+# Add `# type: ignore` to the end of this line to ignore type errors from this import. 
 from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics # type: ignore
 
+# Set up logging structure and start logging. 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', handlers=[logging.FileHandler('app.log'), logging.StreamHandler()])
 logger = logging.getLogger(__name__)
 logging.getLogger("urllib3").setLevel(logging.WARNING)
@@ -17,16 +19,21 @@ logger.info('begin logging...')
 class BusClock:
     def __init__(self):
         logger.info('__init__ begin')
+        # Creating self.url as an empty string to populate later. 
         self.url = []
     
     def setup_display(self):
         logger.info('setup_display begin')
         self.options = RGBMatrixOptions()
+        # These options should be modified depending on the size of your LED matrix. 
         self.options.rows = 32
         self.options.cols = 64
-        self.options.hardware_mapping = 'adafruit-hat'
+        # I am using the adafruit hat to connect my LED matrix to my Raspbery 3 A+.
+        self.options.hardware_mapping = 'adafruit-hat'  
+        # Default is 1, I adjust to 3 for my setup. Read GPIO speed section rpi-rgb-led-matrix module README for more details.
         self.options.gpio_slowdown = 3 
         self.font = graphics.Font()
+        # Create the path to the font file. You can also just pass the path in directly to self.font.LoadFont() and delete the next two lines. 
         script_dir = os.path.dirname(os.path.abspath(__file__))
         font_path = os.path.join(script_dir, 'rpi-rgb-led-matrix', 'fonts', '5x8.bdf')
         self.font.LoadFont(font_path)
@@ -38,7 +45,7 @@ class BusClock:
         logger.info('create_url begin')
         try: 
             config = configparser.ConfigParser()
-            logger.info(os.getcwd())
+            # You must create your own config.ini in the same folder that the app is located in. See directions in README.
             config.read('config.ini')
             api_key = config['UserSettings']['api_key']
             stop_id = config['UserSettings']['stop_id']
@@ -52,7 +59,6 @@ class BusClock:
             self.display_message(messages)
 
     def get_next_bus_time(self):
-        logger.info('get_next_bus_time begin')
         try:
             response = requests.get(self.url)
             response.raise_for_status()
@@ -68,7 +74,6 @@ class BusClock:
             return self.error_message()
         
     def parse_bus_times(self, data):
-        logger.info('parse_bus_times begin')
         try:
             messages = []
             current_time = datetime.now().strftime("%H:%M:%S")
@@ -84,6 +89,7 @@ class BusClock:
                 time_only = aimed_arrival_time.split('T')[1][:8]
                 current_hour, current_minute, _ = map(int, current_time.split(':'))
                 next_bus_hour, next_bus_minute, _ = map(int, time_only.split(':'))
+                # Calculating the times this way does not work if the buss is arriving after midnight, I.E. at 12:05am. It returns a negative number and shows as DELAY. 
                 minutes_until_next_bus = (next_bus_hour - current_hour) * 60 + (next_bus_minute - current_minute)
 
                 if minutes_until_next_bus == 1:
@@ -121,11 +127,14 @@ class BusClock:
 
     def display_message(self, messages):
         self.offscreen_canvas.Clear()
+        # Creates the first line of text. 
         graphics.DrawText(self.offscreen_canvas, self.font, 3, 7, self.textColor, 'M72:')
+        # Creates the second and third lines of text. 
         for index, message in enumerate(messages[:2]):
             graphics.DrawText(self.offscreen_canvas, self.font, 3, 15 + 8 * index, self.textColor, message)
         self.matrix.SwapOnVSync(self.offscreen_canvas)   
 
+    # Currently not used by the application.
     def after_hours(self):
         logger.info('after_hours begin')
         messages = []
